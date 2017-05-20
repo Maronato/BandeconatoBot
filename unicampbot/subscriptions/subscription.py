02278e414ce967fa99cdfc5ffc6aa141b9a9ac5c
Maronato <gustavomaronato@gmail.com>
2017-05-20 02:20:44 -0300

    added extra_info and added emoji to subscription list

--- unicampbot/subscriptions/subscription.py
+++ unicampbot/subscriptions/subscription.py
@@ -1,20 +1,22 @@
 from unicampbot.database.subscriptions_api import get_subscription, set_subscription
 from unicampbot.communication.basic import inline_keyboard_message, edit_message, answer_callback_query
 from telepot.namedtuple import InlineKeyboardMarkup, InlineKeyboardButton
+import emoji
 
 
 class Subscription:
     """docstring for Subscription"""
 
-    def __init__(self, chat_id, callback=False, message_id=False):
+    def __init__(self, chat_id, callback=False, message_id=False, extra_info={'S': ""}):
         self.chat_id = chat_id
         self.message_id = message_id
         self.callback = callback
+        self.extra_info = extra_info
 
     def update_subscription(self, changed):
         subs = self.subs
         subs[changed] = not subs[changed]
-        set_subscription(self.chat_id, subs)
+        set_subscription(self.chat_id, subs, extra=self.extra_info)
         self.get_subs()
         return self
 
@@ -41,23 +43,25 @@ class Subscription:
 
     @property
     def menu_tuple(self):
-        message = "*Inscrições*\n\n\
-        Toque uma opção para se inscrever nela.\n\
-        Toque novamente para se desinscrever.\n\n\
-        As mensagens de café da manhã são enviadas por volta das 6 da manhã.\n\
-        As de almoço por volta das 10 e as de jantar por volta das 16."
+        message = """*Inscrições*\n
+Toque uma opção para se inscrever nela.
+Toque novamente para se desinscrever.\n
+As mensagens de café da manhã são enviadas por volta das 6 da manhã.
+As de almoço por volta das 10 e as de jantar por volta das 16.
+"""
 
         subs = self.subs
+        sub_sign = emoji.emojize(':white_heavy_check_mark:  ', use_aliases=True)
 
         keyboard = InlineKeyboardMarkup(inline_keyboard=[
-            [InlineKeyboardButton(text='Café da manhã' + (' - inscrito' if subs['breakfast'] else ''), callback_data='sub.breakfast')],
+            [InlineKeyboardButton(text=(sub_sign if subs['breakfast'] else '') + 'Café da manhã', callback_data='sub.breakfast')],
             [
-                InlineKeyboardButton(text='Almoço' + (' - inscrito' if subs['lunch'] else ''), callback_data='sub.lunch'),
-                InlineKeyboardButton(text='Almoço veg' + (' - inscrito' if subs['veglunch'] else ''), callback_data='sub.veglunch')
+                InlineKeyboardButton(text=(sub_sign if subs['lunch'] else '') + 'Almoço', callback_data='sub.lunch'),
+                InlineKeyboardButton(text=(sub_sign if subs['veglunch'] else '') + 'Almoço veg', callback_data='sub.veglunch')
             ],
             [
-                InlineKeyboardButton(text='Jantar' + (' - inscrito' if subs['dinner'] else ''), callback_data='sub.dinner'),
-                InlineKeyboardButton(text='Jantar veg' + (' - inscrito' if subs['vegdinner'] else ''), callback_data='sub.vegdinner')
+                InlineKeyboardButton(text=(sub_sign if subs['dinner'] else '') + 'Jantar', callback_data='sub.dinner'),
+                InlineKeyboardButton(text=(sub_sign if subs['vegdinner'] else '') + 'Jantar veg', callback_data='sub.vegdinner')
             ],
         ])
         response_id = self.message_id or self.chat_id
