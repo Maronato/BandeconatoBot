139f3b8c502c1655d87ab5bf5591f86f7e0822ed
Maronato <gustavomaronato@gmail.com>
2017-05-20 02:21:11 -0300

    tests rewritten

--- unicampbot/tests.py
+++ unicampbot/tests.py
@@ -1,5 +1,15 @@
+from telepot import Bot
+from os.path import dirname, abspath
+import os
+
+# set environ variables
+path = dirname(dirname(abspath(__file__))) + "/.env"
+with open(path, 'r') as f:
+    for line in f:
+        line.split("=")
+        os.environ[line.split("=")[0]] = line.split("=")[1].strip()
+bot = Bot(os.environ.get('TELEGRAM_TOKEN'))
 from .core import receiver
-from unicampbot.communication.basic import bot
 
 
 def test():
@@ -9,8 +19,21 @@ def test():
     def maximum_offset(queries, offset):
         for i in queries:
             if i['update_id'] > offset and len(queries) > 1:
-                return maximum_offset(bot.getUpdates(offset=i['update_id']), i['update_id'])
+                offset = i['update_id']
+                return maximum_offset(bot.getUpdates(offset=offset), offset)
             if len(queries) == 1:
-                return i
-    query = maximum_offset(bot.getUpdates(), offset=10)
-    receiver(query)
+                offset = i['update_id']
+                return i, offset
+    offset = 0
+    while True:
+        try:
+            of = offset
+            query, offset = maximum_offset(bot.getUpdates(), offset)
+            if of != offset:
+                receiver(query)
+        except KeyboardInterrupt:
+            break
+
+
+def reset_webhook():
+    return bot.setWebhook(os.environ.get('API_URL'))
