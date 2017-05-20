97f12cba7876894198942389bf6751cf13c7a8be
Maronato <gustavomaronato@gmail.com>
2017-05-20 02:18:37 -0300

    fixed padding and generalized commands

--- unicampbot/commands/commands.py
+++ unicampbot/commands/commands.py
@@ -1,57 +1,54 @@
-from unicampbot.communication.basic import markdown_message
+from unicampbot.communication.basic import markdown_message, custom_keyboard
 from unicampbot.menu.keyboards import day_keyboard
 from unicampbot.subscriptions.subscription import Subscription
 
 
-def start(chat_id):
+def start(chat_id, group=False):
     response = """
-    *"O que vai ter no bandeco hoje?"*
+*"O que vai ter no bandeco hoje?"*
 
-    Essa é uma pergunta que você costuma fazer?
-    Tem preguiça de ir olhar o app da unicamp, ou quer poder receber uma notificação sobre o cardápio do dia?
+Essa é uma pergunta que você costuma fazer?
+Tem preguiça de ir olhar o app da unicamp, ou quer poder receber uma notificação sobre o cardápio do dia?
 
-    _Seus problemas acabaram_
+_Seus problemas acabaram_
 
-    *Comandos*
-    /cardapio - Veja os cardápios
-    /inscrever - Controle suas inscrições
-    /help - Obter ajuda
-    /creditos - Créditos do Bot e acesso ao código
+*Comandos*
+/cardapio - Veja os cardápios
+/inscrever - Controle suas inscrições
+/ajuda - Obter ajuda
+/creditos - Créditos do Bot e acesso ao código
     """
-    markdown_message(chat_id, response)
+    markdown_message(chat_id, response, reply_markup=keyboard(group))
 
 
-def help(chat_id):
+def help(chat_id, group=False):
     response = """
-    *Ajuda*
+*Ajuda*
 
-    Está com problemas para usar o bot?
+A lista de comandos disponíveis é:
+*Comandos*
+/cardapio - Veja os cardápios
+/inscrever - Controle suas inscrições
+/ajuda - Obter ajuda
+/creditos - Créditos do Bot e acesso ao código
 
-    Entre em contato comigo por:
-    @GustavoMaronato
-
-    *Comandos*
-    /cardapio - Veja os cardápios
-    /inscrever - Controle suas inscrições
-    /help - Obter ajuda
-    /creditos - Créditos do Bot e acesso ao código
+Se precisar de suporte, entre em contato com o desenvolvedor:
+@GustavoMaronato
     """
-    markdown_message(chat_id, response)
+    markdown_message(chat_id, response, reply_markup=keyboard(group))
 
 
-def creditos(chat_id):
+def creditos(chat_id, group=False):
     response = """
-    *Créditos*
+*Créditos*
 
-    Esse bot foi feito por
-    @GustavoMaronato
+Esse bot foi feito por
+@GustavoMaronato
 
-    e todo o código fonte dele é aberto sob a licença MIT:
-    https://github.com/Maronato/BandeconatoBot
-
-    Sinta-se livre para colaborar!
+e todo o código fonte dele pode ser encontrado aqui:
+https://github.com/Maronato/BandeconatoBot
     """
-    markdown_message(chat_id, response)
+    markdown_message(chat_id, response, reply_markup=keyboard(group))
 
 
 def cardapio(chat_id):
@@ -60,3 +57,16 @@ def cardapio(chat_id):
 
 def inscrever(chat_id):
     Subscription(chat_id).interact()
+
+
+def unknown(chat_id, group=False):
+    response = "Não reconheço esse comando :(\n\n/ajuda - Lista de comandos"
+    markdown_message(chat_id, response, reply_markup=keyboard(group))
+
+
+def keyboard(group):
+    if not group:
+        reply_markup = custom_keyboard()
+    else:
+        reply_markup = None
+    return reply_markup
