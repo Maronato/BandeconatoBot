d2da80b9a56bf1a138ce84c692def56ee90e53a9
Maronato <gustavomaronato@gmail.com>
2017-05-19 23:05:03 -0300

    generalized message sending

--- unicampbot/communication/basic.py
+++ unicampbot/communication/basic.py
@@ -5,27 +5,25 @@ TOKEN = os.environ.get('TELEGRAM_TOKEN', None)
 bot = telepot.Bot(TOKEN)
 
 
-def simple_message(chat_id, message):
-    bot.sendMessage(chat_id, message)
+def simple_message(chat_id, message, **kwargs):
+    bot.sendMessage(chat_id, message, **kwargs)
 
 
-def markdown_message(chat_id, message):
-    print(chat_id, message)
-    bot.sendMessage(chat_id, message, parse_mode="Markdown")
+def markdown_message(chat_id, message, **kwargs):
+    simple_message(chat_id, message, parse_mode="Markdown", **kwargs)
 
 
-def html_message(chat_id, message):
-    bot.sendMessage(chat_id, message, parse_mode="HTML")
+def html_message(chat_id, message, **kwargs):
+    simple_message(chat_id, message, parse_mode="HTML", **kwargs)
 
 
-def inline_keyboard_message(chat_id, message, keyboard):
-
-    bot.sendMessage(chat_id, message, reply_markup=keyboard, parse_mode="Markdown")
-
-
-def edit_message(message_id, message, reply_markup=None):
-    bot.editMessageText(message_id, message, reply_markup=reply_markup, parse_mode="Markdown")
+def inline_keyboard_message(chat_id, message, keyboard, **kwargs):
+    simple_message(chat_id, message, parse_mode="Markdown", reply_markup=keyboard, **kwargs)
 
 
-def answer_callback_query(query_id, message='', show_alert=False):
-    bot.answerCallbackQuery(query_id, message, show_alert)
+def edit_message(message_id, message, **kwargs):
+    bot.editMessageText(message_id, message, parse_mode="Markdown", **kwargs)
+
+
+def answer_callback_query(query_id, message='', show_alert=False, **kwargs):
+    bot.answerCallbackQuery(query_id, message, show_alert, **kwargs)
